21fd7fe08c777b0fb340e6bb3a5120569d81e9a4
mandy723 <m816866562000@gmail.com>
2018-06-27 15:34:28 +0800

    modify file

--- kkbox_search_test.py
+++ kkbox_search_test.py
@@ -6,24 +6,26 @@ import unittest, time
 
 #創建一個引用 unittest 的測試用例
 class KKBOXTestCase(unittest.TestCase):
+    #測試前準備環境的搭建
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://play.kkbox.com")
         # login
-        self.driver.find_element_by_id("uid").send_keys("XXXXXXX@kkbox.com")
-        self.driver.find_element_by_id("pwd").send_keys("XXXXXXX")
+        self.driver.find_element_by_id("uid").send_keys("mandyyang@kkbox.com")
+        self.driver.find_element_by_id("pwd").send_keys("P299kkbox")
         self.driver.find_element_by_id("login-btn").send_keys(Keys.ENTER)
         time.sleep(2)
-
+    #測試後環境的還原
     def tearDown(self):
         self.driver.close() 
 
 
     def test_search_artist(self):
+        """搜尋歌手:韋禮安"""
         driver = self.driver
-        driver.find_element_by_tag_name("input").send_keys(u"韋禮安")
+        driver.find_element_by_css_selector(".search_hint").send_keys(u"韋禮安")
         time.sleep(1)
-        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        driver.find_element_by_css_selector(".search_hint").send_keys(Keys.ENTER)
         time.sleep(2)
         search_results = driver.find_elements_by_css_selector("ul.cards.artists")
         time.sleep(1)
@@ -32,27 +34,27 @@ class KKBOXTestCase(unittest.TestCase):
 
 
     def test_search_song(self):
+        """搜尋歌曲:因為愛"""
         driver = self.driver
-        driver.find_element_by_tag_name("input").send_keys(u"因為愛")
+        driver.find_element_by_css_selector(".search_hint").send_keys(u"因為愛")
         time.sleep(1)
-        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        driver.find_element_by_css_selector(".search_hint").send_keys(Keys.ENTER)
         time.sleep(2)
-        # 取得 song table
-        song_table = driver.find_element_by_tag_name("table")
-        # 取得每一列（tr:橫向）
-        rows = song_table.find_elements_by_tag_name("tr")
-        # 第一列是項目名稱，不需要，所以只取第二列到最後一列 [1:]
-        for row in rows[1:]:
-            # 取得第二欄（td:直向），歌曲名稱
+        # 取得 song table, 每一列（tr:橫向）
+        song_table = driver.find_elements_by_css_selector(".songs-table tr")
+        # 第一列是項目名稱, 不需要, 所以只取第二列到最後一列 [1:]
+        for row in song_table[1:]:
+            # 取得第二欄（td:直向）, 歌曲名稱
             search_result = row.find_elements_by_tag_name("td")[1]
             self.assertIn(u"因為愛", search_result.text)
 
 
     def test_search_song_list(self):
+        """搜尋歌單:韋禮安 (William Wei) 歷年精選"""
         driver = self.driver
-        driver.find_element_by_tag_name("input").send_keys(u"韋禮安 (William Wei) 歷年精選")
+        driver.find_element_by_css_selector(".search_hint").send_keys(u"韋禮安 (William Wei) 歷年精選")
         time.sleep(1)
-        driver.find_element_by_tag_name("input").send_keys(Keys.ENTER)
+        driver.find_element_by_css_selector(".search_hint").send_keys(Keys.ENTER)
         time.sleep(2)
         search_results = driver.find_elements_by_css_selector("ol.cards")
         time.sleep(1)
